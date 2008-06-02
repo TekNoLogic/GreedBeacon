be1717dcca784a21a2888e3faefbab8743c18e13
Tekkub <tekkub@gmail.com>
2008-06-01 18:15:04 -0600

    Set up for deDE debuggery

--- GreedBeacon.lua
+++ GreedBeacon.lua
@@ -1,5 +1,11 @@
+local YOU = "Ihr"; -- "You", the player
+local GREED = "Gier";
+local NEED = "Bedarf";
 
-local L = setmetatable({}, {__index = function(t,i) return i end})
+--~ local L = setmetatable(GetLocale() == "deDE" and {
+local L = setmetatable({
+}, {__index = function(t,i) return i end})
+--~ } or {}, {__index = function(t,i) return i end})
 
 local colorneed, colorgreed = "|cffff0000", "|cffffff00"
 local coloredwords = {Greed = colorgreed.."Greed", Need = colorneed.."Need"}
@@ -7,8 +13,7 @@ local rolls = {}
 
 local function Print(...) ChatFrame1:AddMessage(string.join(" ", "|cFF33FF99GreedBeacon|r:", ...)) end
 
-local debugf = tekDebug and tekDebug:GetFrame("GreedBeacon")
-local function Debug(...) if debugf then debugf:AddMessage(string.join(", ", ...)) end end
+local function Debug(...) ChatFrame1:AddMessage(string.join(", ", ...)) end
 
 
 local chatframes = {[ChatFrame1] = false, [ChatFrame2] = false, [ChatFrame3] = false, [ChatFrame4] = false, [ChatFrame5] = false, [ChatFrame6] = false, [ChatFrame7] = false}
@@ -88,12 +93,13 @@ f:SetScript("OnEvent", function(self, event, msg)
 end)
 
 
-ChatFrame_AddMessageEventFilter("CHAT_MSG_LOOT", function(msg)
+local function filter(msg)
 	if msg:match("(.*) won: (.+)") or msg:match(" has?v?e? selected .+ for: ") or msg:match(" Roll . %d+ for .+ by ") or (msg:match(" passed on: ") and not msg:match("Everyone passed on: ")) then
 		Debug("Supressing chat message", msg)
 		return true
 	end
-end)
+end
+ChatFrame_AddMessageEventFilter("CHAT_MSG_LOOT", filter)
 
 
 local orig2 = SetItemRef
@@ -112,3 +118,56 @@ function SetItemRef(link, text, button)
 		ItemRefTooltip:Show()
 	else return orig2(link, text, button) end
 end
+
+
+-- Debugging event generator
+local oe = f:GetScript("OnEvent")
+local function e(msg)
+	Print(msg)
+	oe(f, "CHAT_MSG_LOOT", msg)
+	filter(msg)
+end
+
+local LOOT_ROLL_ALL_PASSED = "Alle haben gepasst bei: %s";
+local LOOT_ROLL_GREED =        "%s hat f\195\188r %s 'Gier' ausgew\195\164hlt";
+local LOOT_ROLL_GREED_SELF = "Ihr habt f\195\188r %s 'Gier' ausgew\195\164hlt";
+local LOOT_ROLL_NEED =         "%s hat f\195\188r %s 'Bedarf' ausgew\195\164hlt";
+local LOOT_ROLL_NEED_SELF =  "Ihr habt f\195\188r %s 'Bedarf' ausgew\195\164hlt";
+local LOOT_ROLL_PASSED =      "%s w\195\188rfelt nicht f\195\188r: %s";
+local LOOT_ROLL_PASSED_SELF =   "Ihr habt gepasst bei: %s";
+local LOOT_ROLL_PASSED_AUTO =        "%1$s passt automatisch bei %2$s, weil er den Gegenstand nicht benutzen kann.";
+local LOOT_ROLL_PASSED_AUTO_FEMALE = "%1$s passt automatisch bei %2$s, weil sie den Gegenstand nicht benutzen kann.";
+local LOOT_ROLL_PASSED_SELF_AUTO = "Ihr passt automatisch bei %s, weil Ihr den Gegenstand nicht benutzen k\195\182nnt.";
+local LOOT_ROLL_ROLLED_GREED =  "Wurf f\195\188r Gier: %d f\195\188r %s von %s"; -- First %s is item, second %s is player name
+local LOOT_ROLL_ROLLED_NEED = "Wurf f\195\188r Bedarf: %d f\195\188r %s von %s"; -- First %s is item, second %s is player name
+local LOOT_ROLL_WON = "%s gewinnt: %s";
+local LOOT_ROLL_WON_NO_SPAM_GREED = "%1$s gewinnt: %3$s |cff818181(Gier - %2$d)|r";
+local LOOT_ROLL_WON_NO_SPAM_NEED = "%1$s gewinnt: %3$s |cff818181(Bedarf - %2$d)|r";
+local LOOT_ROLL_YOU_WON = "Ihr gewinnt: %s";
+local YOU = "Ihr"; -- "You", the player
+
+local _, link = GetItemInfo(6948)
+e(string.format(LOOT_ROLL_NEED, "Joe", link))
+e(string.format(LOOT_ROLL_NEED_SELF, link))
+e(string.format(LOOT_ROLL_GREED, "Bob", link))
+e(string.format(LOOT_ROLL_PASSED, "Ike", link))
+e(string.format(LOOT_ROLL_ROLLED_NEED, 85, link, "Joe"))
+e(string.format(LOOT_ROLL_ROLLED_NEED, 8, link, UnitName("player")))
+e(string.format(LOOT_ROLL_WON, "Joe", link))
+
+local _, link = GetItemInfo(33809)
+e(string.format(LOOT_ROLL_GREED, "Joe", link))
+e(string.format(LOOT_ROLL_GREED_SELF, link))
+e(string.format(LOOT_ROLL_GREED, "Bob", link))
+e(string.format(LOOT_ROLL_PASSED, "Ike", link))
+e(string.format(LOOT_ROLL_ROLLED_GREED, 42, link, "Joe"))
+e(string.format(LOOT_ROLL_ROLLED_GREED, 99, link, UnitName("player")))
+e(string.format(LOOT_ROLL_ROLLED_GREED, 70, link, "Bob"))
+e(string.format(LOOT_ROLL_ROLLED_GREED, 91, link, "Ike"))
+e(string.format(LOOT_ROLL_WON, YOU, link))
+
+local _, link = GetItemInfo(11411)
+e(string.format(LOOT_ROLL_PASSED_AUTO, "Joe", link))
+e(string.format(LOOT_ROLL_PASSED_AUTO_FEMALE, "May", link))
+e(string.format(LOOT_ROLL_PASSED_SELF, link))
+e(string.format(LOOT_ROLL_ALL_PASSED, link))
