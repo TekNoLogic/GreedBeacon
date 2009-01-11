05c6a5cc8fade17aff3743174ab2d11cf53f8bfe
Tekkub <tekkub@gmail.com>
2009-01-10 20:48:20 -0700

    Add config instead of relying on Blizzy's somewhat buggy chat settings

---
+++ Config.lua
@@ -0,0 +1,46 @@
+
+---------------------
+--      Panel      --
+---------------------
+
+local frame = CreateFrame("Frame", nil, InterfaceOptionsFramePanelContainer)
+frame.name = "GreedBeacon"
+frame:Hide()
+frame:SetScript("OnShow", function(frame)
+	local title, subtitle = LibStub("tekKonfig-Heading").new(frame, "GreedBeacon", "This panel allows you to control which chat frame GreedBeacon outputs to.")
+
+
+	local chatframedropdown, chatframedropdowntext, chatframedropdowncontainer = LibStub("tekKonfig-Dropdown").new(frame, "Output to", "TOPLEFT", subtitle, "BOTTOMLEFT", -2, -8)
+	chatframedropdowntext:SetText(GreedBeaconDB.frame)
+	chatframedropdown.tiptext = "Select which chat frame to output to."
+
+	local function OnClick(self)
+		UIDropDownMenu_SetSelectedValue(chatframedropdown, self.value)
+		chatframedropdowntext:SetText(self.value)
+		GreedBeaconDB.frame = self.value
+	end
+	UIDropDownMenu_Initialize(chatframedropdown, function()
+		local selected, info = UIDropDownMenu_GetSelectedValue(chatframedropdown) or GreedBeaconDB.frame, UIDropDownMenu_CreateInfo()
+
+		for i=1,7 do
+			info.text = "ChatFrame"..i
+			info.value = "ChatFrame"..i
+			info.func = OnClick
+			info.checked = ("ChatFrame"..i) == selected
+			UIDropDownMenu_AddButton(info)
+		end
+	end)
+
+
+	local ident = LibStub("tekKonfig-Button").new_small(frame, "TOP", chatframedropdown, "TOP")
+	ident:SetPoint("RIGHT", -16, 0)
+	ident:SetText("Identify")
+	ident.tiptext = "Identify each chat frame"
+	ident:SetScript("OnClick", function(self) for i=1,7 do _G["ChatFrame"..i]:AddMessage("|cFF33FF99GreedBeacon|r: This is ChatFrame"..i) end end)
+
+
+	frame:SetScript("OnShow", nil)
+end)
+
+InterfaceOptions_AddCategory(frame)
+LibStub("tekKonfig-AboutPanel").new("GreedBeacon", "GreedBeacon")
--- GreedBeacon.lua
+++ GreedBeacon.lua
@@ -25,39 +25,12 @@ local L = setmetatable(GetLocale() == "deDE" and {
 
 local colorneed, colorgreed = "|cffff0000", "|cffffff00"
 local coloredwords = {[L.Greed] = colorgreed..L.Greed, [L.Need] = colorneed..L.Need}
-local rolls = {}
+local rolls, db = {}
 
-local function Print(...) ChatFrame1:AddMessage(string.join(" ", "|cFF33FF99GreedBeacon|r:", ...)) end
+local function Print(...) print("|cFF33FF99GreedBeacon|r:", ...) end
 
 local debugf = tekDebug and tekDebug:GetFrame("GreedBeacon")
 local function Debug(...) if debugf then debugf:AddMessage(string.join(", ", ...)) end end
-
-
-local chatframes = {[ChatFrame1] = false, [ChatFrame2] = false, [ChatFrame3] = false, [ChatFrame4] = false, [ChatFrame5] = false, [ChatFrame6] = false, [ChatFrame7] = false}
-for frame in pairs(chatframes) do
-	for i,v in pairs(frame.messageTypeList) do
-		if v == "LOOT" then
-			chatframes[frame] = true
-			Debug("Initializing", frame:GetName())
-		end
-	end
-end
-
-local origadd, origrem = ChatFrame_AddMessageGroup, ChatFrame_RemoveMessageGroup
-ChatFrame_AddMessageGroup = function(frame, channel, ...)
-	if channel == "LOOT" then
-		chatframes[frame] = true
-		Debug("ChatFrame_AddMessageGroup", frame:GetName())
-	end
-	return origadd(frame, channel, ...)
-end
-ChatFrame_RemoveMessageGroup = function(frame, channel, ...)
-	if channel == "LOOT" then
-		chatframes[frame] = false
-		Debug("ChatFrame_RemoveMessageGroup", frame:GetName())
-	end
-	return origrem(frame, channel, ...)
-end
 
 
 local function FindRoll(link, player, hasselected)
@@ -72,41 +45,58 @@ end
 
 
 local f = CreateFrame("Frame")
-f:RegisterEvent("CHAT_MSG_LOOT")
-f:SetScript("OnEvent", function(self, event, msg)
-	local rolltype, rollval, link, player = msg:match(L["(.+) Roll . (%d+) for (.+) by (.+)"])
-	if player then
-		local roll = FindRoll(link, player, true)
-		Debug("Roll detected", player, rolltype, rollval, link)
-		roll[player] = (rolltype == "Need" and colorneed or colorgreed)..rollval
-		roll._type = rolltype
-		return
-	end
+f:RegisterEvent("ADDON_LOADED")
+f:SetScript("OnEvent", function(self, event, addon)
+	if addon:lower() ~= "greedbeacon" then return end
 
-	local player, selection, link = msg:match(L["(.*) has?v?e? selected (.+) for: (.+)"])
-	if player and player ~= "" then
-		player = player == YOU and UnitName("player") or player
-		Debug("Selection detected", player, selection, link)
-		FindRoll(link, player)[player] = coloredwords[selection]
-		return
-	end
-
-	local player, link = msg:match(L["(.*) won: (.+)"])
-	if player then
-		player = player == YOU and UnitName("player") or player
-		for i,roll in ipairs(rolls) do
-			if roll._link == link and roll[player] and not roll._printed then
-				roll._printed = true
-				roll._winner = player
-				Debug("Roll completed", roll._type or "nil", i, player, link)
-				local msg = string.format(L["%s|Hgreedbeacon:%d|h[%s roll]|h|r %s won %s "], roll._type == L.Need and colorneed or colorgreed, i, roll._type or "???", player, link)
-				for frame,val in pairs(chatframes) do if val then frame:AddMessage(msg) end end
-				return
-			end
+	GreedBeaconDB = GreedBeaconDB or {}
+	db = GreedBeaconDB
+	if not db.frame then
+		db.frame = "ChatFrame1"
+		for i=7,1,-1 do
+			local name = "ChatFrame"..i
+			for i,v in pairs(_G[name].messageTypeList) do if v == "LOOT" then db.frame = name end end
 		end
-		Print("No match found for", msg)
-		return
+		Debug("Initializing DB to", db.frame)
 	end
+
+	f:UnregisterEvent("ADDON_LOADED")
+	f:RegisterEvent("CHAT_MSG_LOOT")
+	f:SetScript("OnEvent", function(self, event, msg)
+		local rolltype, rollval, link, player = msg:match(L["(.+) Roll . (%d+) for (.+) by (.+)"])
+		if player then
+			local roll = FindRoll(link, player, true)
+			Debug("Roll detected", player, rolltype, rollval, link)
+			roll[player] = (rolltype == "Need" and colorneed or colorgreed)..rollval
+			roll._type = rolltype
+			return
+		end
+
+		local player, selection, link = msg:match(L["(.*) has?v?e? selected (.+) for: (.+)"])
+		if player and player ~= "" then
+			player = player == YOU and UnitName("player") or player
+			Debug("Selection detected", player, selection, link)
+			FindRoll(link, player)[player] = coloredwords[selection]
+			return
+		end
+
+		local player, link = msg:match(L["(.*) won: (.+)"])
+		if player then
+			player = player == YOU and UnitName("player") or player
+			for i,roll in ipairs(rolls) do
+				if roll._link == link and roll[player] and not roll._printed then
+					roll._printed = true
+					roll._winner = player
+					Debug("Roll completed", roll._type or "nil", i, player, link)
+					local msg = string.format(L["%s|Hgreedbeacon:%d|h[%s roll]|h|r %s won %s "], roll._type == L.Need and colorneed or colorgreed, i, roll._type or "???", player, link)
+					_G[db.frame]:AddMessage(msg)
+					return
+				end
+			end
+			Print("ERROR: No match found for", msg)
+			return
+		end
+	end)
 end)
 
 
